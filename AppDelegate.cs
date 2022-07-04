58c2440c92bd50e558967ab4d86a898ee9c287d5
mrpranjal001 <105543761+mrpranjal001@users.noreply.github.com>
2022-07-04 11:06:48 +0600

    Add files via upload

---
+++ AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace PrescriptionFiller.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+            App.ScreenWidth = (int)(UIScreen.MainScreen.Bounds.Width * UIScreen.MainScreen.Scale);
+            App.ScreenHeight = (int)(UIScreen.MainScreen.Bounds.Height * UIScreen.MainScreen.Scale);
+            App.ScreenDensity = (float)UIScreen.MainScreen.Scale;
+
+			// Code for starting up the Xamarin Test Cloud Agent
+			#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+			#endif
+
+			LoadApplication (new App ());
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
